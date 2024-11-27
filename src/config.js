29485f57cd05ec7ec287a422d39b61188d5f1df1
Abdykali Azat <azat.abdykalli@gmail.com>
2024-11-27 16:23:05 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://cargotrek.kz:3001',
+    
+    nameCargo: 'Cargo Tracking'
+  };
+  
+export default config;
+  
